c88f64e9526e4115adab4393781d5aa1ba8c9619
joe <joe@goodfootrecords.co.uk>
2011-12-04 17:40:32 +0000

    Creation of Komodo Project

---
+++ MinifyEverythin.py
@@ -0,0 +1,19 @@
+!#/usr/bin/python
+
+def process_js(raw_js):
+    pass
+
+def process_css(raw_css):
+    pass
+
+def process_html(raw_html):
+    pass
+
+def file_type(path):
+    """
+    Return the type of file based on it's extension and doctype (where applicable)
+    """
+    pass
+
+
+if __name__ == '__main__': main()
